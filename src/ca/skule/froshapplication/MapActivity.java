ebf468d52ac36241ff0131dffa4e02bb5291c717
Tom Zhang <tomzhang94@gmail.com>
2013-07-07 21:57:59 -0400

    Forgot the mapActivity

---
+++ src/ca/skule/froshapplication/MapActivity.java
@@ -0,0 +1,36 @@
+package ca.skule.froshapplication;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MapActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		getActionBar().setDisplayShowTitleEnabled(false);
+		return true;
+	}
+
+	public void onMenuClick (MenuItem Item){
+		String id = Item.getTitle().toString();
+		if (id.equalsIgnoreCase("schedule")){
+			// Point to Schedule
+		}
+		else if (id.equalsIgnoreCase("map")){
+			// Point to Map
+		}
+	}
+}
