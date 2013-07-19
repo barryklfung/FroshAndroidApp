f696977603110963fa79793bd7ad1dc2a78149c5
Tom Zhang <tomzhang94@gmail.com>
2013-07-19 17:40:14 -0400

    added my R.java since java kept on complaining.

--- gen/ca/skule/froshapplication/R.java
+++ gen/ca/skule/froshapplication/R.java
@@ -9,6 +9,14 @@ package ca.skule.froshapplication;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int map=0x7f010000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,37 +25,46 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int u_of_t_map=0x7f020001;
     }
     public static final class id {
-        public static final int list=0x7f080002;
-        public static final int map=0x7f080001;
-        public static final int schedule=0x7f080000;
+        public static final int area1001=0x7f090002;
+        public static final int area1002=0x7f090003;
+        public static final int area1004=0x7f090004;
+        public static final int area1005=0x7f090005;
+        public static final int area1006=0x7f090006;
+        public static final int area1007=0x7f090007;
+        public static final int list=0x7f090009;
+        public static final int map=0x7f090000;
+        public static final int schedule=0x7f090008;
+        public static final int zoomview=0x7f090001;
     }
     public static final class layout {
         public static final int activity_list=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_map=0x7f030002;
+        public static final int activity_map2=0x7f030003;
+        public static final int activity_map3=0x7f030004;
     }
     public static final class menu {
-        public static final int list=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int map=0x7f070002;
+        public static final int list=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int map=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050006;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050007;
-        public static final int list=0x7f050004;
-        public static final int map=0x7f050002;
-        public static final int schedule=0x7f050001;
-        public static final int title_activity_list=0x7f050005;
-        public static final int title_activity_map=0x7f050003;
+        public static final int action_settings=0x7f060006;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060007;
+        public static final int list=0x7f060004;
+        public static final int map=0x7f060002;
+        public static final int schedule=0x7f060001;
+        public static final int title_activity_list=0x7f060005;
+        public static final int title_activity_map=0x7f060003;
     }
     public static final class style {
         /** 
@@ -71,10 +88,42 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
+    public static final class xml {
+        public static final int maps=0x7f040000;
+    }
+    public static final class styleable {
+        /** Attributes that can be used with a ImageMap.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ImageMap_map ca.skule.froshapplication:map}</code></td><td></td></tr>
+           </table>
+           @see #ImageMap_map
+         */
+        public static final int[] ImageMap = {
+            0x7f010000
+        };
+        /**
+          <p>This symbol is the offset where the {@link ca.skule.froshapplication.R.attr#map}
+          attribute's value can be found in the {@link #ImageMap} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:map
+        */
+        public static final int ImageMap_map = 0;
+    };
 }
