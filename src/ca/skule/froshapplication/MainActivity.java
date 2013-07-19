c6e6c8195d9815a0fe44eeb89c7c0c7a8513267c
Tom Zhang <tomzhang94@gmail.com>
2013-07-19 19:03:31 -0400

    Fixed the merged

--- src/ca/skule/froshapplication/MainActivity.java
+++ src/ca/skule/froshapplication/MainActivity.java
@@ -1,46 +1,3 @@
-/*<<<<<<< HEAD
-package ca.skule.froshapplication;
-
-import android.os.Build;
-import android.os.Bundle;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		getActionBar().setDisplayShowTitleEnabled(false);
-		return true;
-	}
-
-	public void onMenuClick (MenuItem Item){
-		String id = Item.getTitle().toString();
-		if (id.equalsIgnoreCase("Schedule")){
-			Intent intent = new Intent (this, DayListActivity.class);
-			startActivity (intent);
-		}
-		else if (id.equalsIgnoreCase("Map")){
-			Intent intent = new Intent (this, MapActivity.class);
-			startActivity(intent);
-		}
-	}
-}
-*/
-
-//=======
 package ca.skule.froshapplication;
 
 import android.os.Build;
@@ -86,4 +43,3 @@ public class MainActivity extends Activity {
 	}
 }
 
-//>>>>>>> 38eaaa7d467ea52c65dcd28627ccc3a62c86f305
