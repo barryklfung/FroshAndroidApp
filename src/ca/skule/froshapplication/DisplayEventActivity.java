2e49ee959f27cadda9a528acd848235581ce711a
King Fung <king.fung@mail.utoronto.ca>
2013-07-19 21:48:03 -0400

    Modified Action Bar for schedule activities

--- src/ca/skule/froshapplication/DisplayEventActivity.java
+++ src/ca/skule/froshapplication/DisplayEventActivity.java
@@ -1,9 +1,12 @@
 package ca.skule.froshapplication;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
@@ -45,11 +48,23 @@ public class DisplayEventActivity extends Activity {
 		startActivity(intent);
 	}
 
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.display_event, menu);
+		getActionBar().setDisplayShowTitleEnabled(false);
 		return true;
 	}
-
+	public void onMenuClick (MenuItem Item){
+		String id = Item.getTitle().toString();
+		if (id.equalsIgnoreCase("Schedule")){
+			Intent intent = new Intent (this, DayListActivity.class);
+			startActivity (intent);
+		}
+		else if (id.equalsIgnoreCase("Map")){
+			Intent intent = new Intent (this, MapActivity.class);
+			startActivity(intent);
+		}
+	}
 }
--- src/ca/skule/froshapplication/EventListActivity.java
+++ src/ca/skule/froshapplication/EventListActivity.java
@@ -1,8 +1,11 @@
 package ca.skule.froshapplication;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -53,11 +56,23 @@ public class EventListActivity extends Activity {
 		startActivity(intent);
 	}
 
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.event_list, menu);
+		getActionBar().setDisplayShowTitleEnabled(false);		
 		return true;
 	}
-
+	public void onMenuClick (MenuItem Item){
+		String id = Item.getTitle().toString();
+		if (id.equalsIgnoreCase("Schedule")){
+			Intent intent = new Intent (this, DayListActivity.class);
+			startActivity (intent);
+		}
+		else if (id.equalsIgnoreCase("Map")){
+			Intent intent = new Intent (this, MapActivity.class);
+			startActivity(intent);
+		}
+	}
 }
