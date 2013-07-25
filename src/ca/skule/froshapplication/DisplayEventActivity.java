f48ae9eb31fcb9e24d67dea8a82491b99663b95d
masterbruce <andrew_lim94@hotmail.com>
2013-07-25 14:44:01 -0400

    first steps to local notifications - can set for a specific time (doesn't do anything on click yet)

--- src/ca/skule/froshapplication/DisplayEventActivity.java
+++ src/ca/skule/froshapplication/DisplayEventActivity.java
@@ -1,13 +1,19 @@
 package ca.skule.froshapplication;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
-import android.widget.TextView;
-import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DisplayEventActivity extends Activity {
 
@@ -19,6 +25,7 @@ public class DisplayEventActivity extends Activity {
 		setContentView(R.layout.activity_display_event);
 		
 		Button button = (Button)findViewById(R.id.mapButton);
+		Button localButton = (Button)findViewById(R.id.local_notify_button);
 		
 		
 		Intent intent = getIntent();
@@ -38,13 +45,35 @@ public class DisplayEventActivity extends Activity {
 			}
 		});
 		
+		localButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick (View view){
+				runLocalNotiIntent();
+			}
+		});
+		
 	}
 	private void runIntent (){
 		Intent intent = new Intent (this, MapActivity.class);
 		intent.putExtra("Location", thisEvent.getLocation());
 		startActivity(intent);
 	}
-
+	
+	private void runLocalNotiIntent (){	        
+	        Intent notificationIntent = 
+	            new Intent(DisplayEventActivity.this, NotificationService.class);
+	        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+	        PendingIntent contentIntent = 
+	            PendingIntent.getService(this, 0, notificationIntent, 0);
+	        Calendar calendar = Calendar.getInstance();
+	           calendar.set(Calendar.HOUR_OF_DAY, 16);
+	       calendar.set(Calendar.MINUTE, 41);
+	       calendar.set(Calendar.SECOND, 00);
+	       
+	       alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), contentIntent);  //set repeating every 24 hours
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
